fca4780fed5218bf0135b55c19a6175e2ed4f241
Salla THIAM <sallayama.th@icloud.com>
2025-12-10 01:04:40 +0000

    fix smtp

--- ANG_AVI/settings.py
+++ ANG_AVI/settings.py
@@ -144,7 +144,7 @@ EMAIL_BACKEND = config("EMAIL_BACKEND", default="django.core.mail.backends.smtp.
 EMAIL_HOST = config("EMAIL_HOST")
 EMAIL_PORT = config("EMAIL_PORT", cast=int)
 EMAIL_USE_TLS = config("EMAIL_USE_TLS", cast=bool)
-DEFAULT_FROM_EMAIL = config("EMAIL_HOST_USER")
+DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL")
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
 
--- Backend/views/contact_message_view.py
+++ Backend/views/contact_message_view.py
@@ -45,27 +45,3 @@ def send_contact_email(request):
         status=405
     )
 
-
-# def send_contact_email(request):
-#     if request.method == 'POST':
-#         data = json.loads(request.body)
-#         name = data.get('name')
-#         email = data.get('email')   
-#         subject = data.get('subject')
-#         message = data.get('message')
-
-#         full_message = f"De: {name} <{email}>\n\n{message}"
-
-#         email_message = EmailMessage(
-#             subject=subject,
-#             body=full_message,
-#             from_email=settings.DEFAULT_FROM_EMAIL,
-#             to=[settings.DEFAULT_FROM_EMAIL],
-#             reply_to=[email] if email else None
-#         )
-
-#         email_message.send(fail_silently=False)
-
-#         return JsonResponse({"status": True, "message": "Email envoyé avec succès."})
-
-#     return JsonResponse({"status": False, "message": "Méthode non autorisée."}, status=405)
